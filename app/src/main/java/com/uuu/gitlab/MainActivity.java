3e203efbf11373753f9f61a4a67d132473586407
mark Ho <mark@uuu.com>
2017-07-26 14:18:49 +0800

    Change UI theme

--- app/src/main/java/com/uuu/gitlab/MainActivity.java
+++ app/src/main/java/com/uuu/gitlab/MainActivity.java
@@ -1,7 +1,9 @@
 package com.uuu.gitlab;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView=(TextView) findViewById(R.id.textView1);
+        textView.setTextSize(32);
+        textView.setTextColor(Color.BLUE);
+        textView.setText("Hello Git");
+
+
     }
 }
